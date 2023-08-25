93094eb9cca988dc2f1366c0ceeb7763261130e5
bce0404 <bce0404@midasit.com>
2023-08-25 16:06:38 +0900

    local axis 작업 중 - MUI 일부 제외하고 컴포넌트 대체 완료 / 검증 필요 / Modal, Dialog, Box? 작업 필요

--- sample_src/local_axis-moaui/src/Components/Modal.js
+++ sample_src/local_axis-moaui/src/Components/Modal.js
@@ -1,72 +1,54 @@
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
-import * as Buttons from './Buttons.js';
-import Stack from '@mui/material/Stack';
-import HelpImage from '../svg/help.svg'
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import * as Buttons from "./Buttons.js";
 
-export {NodeImporModal, HelpModal}
+import Dialog from "@mui/material/Dialog";
+import MoaPanel from "@midasit-dev/moaui/Panel";
+import MoaStack from "@midasit-dev/moaui/Stack";
+import MoaButton from "@midasit-dev/moaui/Button";
+import MoaTextField from "@midasit-dev/moaui/TextField";
 
-function NodeImporModal(openModal, closeModal, setNode, clickevent) {
-  return (
-    <Modal
-      open={openModal}
-      onClose={closeModal}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        bgcolor: 'background.paper',
-        width: 420,
-        height: 100,
-        border: '0px solid #000',
-        borderRadius: '5px',
-        boxShadow: 24,
-      }}>
-      <Stack spacing={1} direction="row" alignItems="center" sx={{margin:4, marginLeft:8}}>
-        <TextField
-          focused 
-          required
-          size="small"
-          id="outlined-required"
-          label="Node"
-          onChange={(e)=>setNode(e.target.value)}
-          sx={{padding:0, margin:4}}
-          inputProps={{style: {fontSize: "14px"}}}
-        />
-        {Buttons.NodeButton("contained","apply",clickevent)}
-      </Stack>
-      </Box>
-    </Modal>
-  )
+import HelpImage from "../svg/help.svg";
+
+export { NodeImportDialog, HelpDialog };
+
+function NodeImportDialog(openDialog, onCloseDialog, setNode, clickevent) {
+	return (
+		<Dialog
+			open={openDialog}
+			onClose={onCloseDialog}
+			aria-labelledby="Node Import Dialog"
+			aria-describedby="Node Import Dialog"
+		>
+			<MoaStack>
+				<MoaPanel>
+					<MoaStack spacing={1} direction="row" alignItems="center">
+						<MoaTextField
+							focused
+							required
+							size="small"
+							id="outlined-required"
+							title="Node"
+							titlePosition="left"
+							onChange={(e) => setNode(e.target.value)}
+						/>
+						<MoaButton variant="contained" onClick={clickevent}>APPLY</MoaButton>
+					</MoaStack>
+				</MoaPanel>
+			</MoaStack>
+		</Dialog>
+	);
 }
 
-function HelpModal(openModal, closeModal) {
-  return (
-    <Modal
-      open={openModal}
-      onClose={closeModal}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        bgcolor: 'background.paper',
-        width: 691*0.8,
-        height: 615*0.8,
-        border: '0px solid #000',
-        borderRadius: '5px',
-        boxShadow: 24,
-      }}>
-        <img className="imageStyle" src={HelpImage} alt="" />
-      </Box>
-    </Modal>
-  )
+function HelpDialog(openDialog, onCloseDialog) {
+	return (
+		<Dialog
+			open={openDialog}
+			onClose={onCloseDialog}
+			aria-labelledby="Monotone cubic interpolation help dialog"
+			aria-describedby="Monotone cubic interpolation graph"
+		>
+			<img src={HelpImage} alt="Monotone cubic interpolation help" width={691 * 0.8} height={615 * 0.8} />
+		</Dialog>
+	);
 }
--- sample_src/local_axis-moaui/src/Components/RadioGroup.js
+++ sample_src/local_axis-moaui/src/Components/RadioGroup.js
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import { Typography } from '@mui/material';
 
-function StyledRadio() {
-  return(
-    <Radio size="small" />
-  )
-}
+import MoaRadioGroup from '@midasit-dev/moaui/RadioGroup';
+import MoaRadioButton from '@midasit-dev/moaui/Radio';
 
 export default function RadioButtonsGroup(defaultOp,setValue) {
   
@@ -18,18 +10,15 @@ export default function RadioButtonsGroup(defaultOp,setValue) {
   }
   
   return (
-    <FormControl>
-      <RadioGroup
-        aria-labelledby="demo-radio-buttons-group-label"
-        value={defaultOp}
-        name="radio-buttons-group"
-        onChange={handleChange}
-        sx = {{marginLeft:"10px", marginTop:"5px", marginBottom:"5px"}}
-      >
-        <FormControlLabel value="MCS" control={StyledRadio()} label={<Typography sx={{fontSize:"14px"}}>Monotone Cubic Hermite Spline</Typography>} />
-        <FormControlLabel value="NCS" control={StyledRadio()} label={<Typography sx={{fontSize:"14px"}}>Natural Cubic Spline</Typography>} />
-        <FormControlLabel value="CCS" control={StyledRadio()} label={<Typography sx={{fontSize:"14px"}}>Clamped Cubic Spline</Typography>} />
-      </RadioGroup>
-    </FormControl>
+	<MoaRadioGroup
+		name="Cubic Spline Radio Group"
+		aria-labelledby="Cubic Spline Radio Group Label"
+		onChange={handleChange}
+		value={defaultOp}
+	>
+		<MoaRadioButton value="MCS" name="Monotone Cubic Hermite Spline" />
+		<MoaRadioButton value="NCS" name="Natural Cubic Spline" />
+		<MoaRadioButton value="CCS" name="Clamped Cubic Spline" />
+	</MoaRadioGroup>
   );
 }
